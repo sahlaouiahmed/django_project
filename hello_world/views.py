b439d8ef3588c7cd48a8781264ecbaa163707f3b
Ahmed Sahlaoui <sahlaouiahmad@outlook.com>
2024-10-02 19:40:37 +0000

    updating the hello_world function

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index (request):
-    return HttpResponse("Hello, world!")
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
 
 
